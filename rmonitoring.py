c0433c70e33d2668c0bb104293516c7f64307654
Sergei <60666684+sambelony@users.noreply.github.com>
2022-08-20 14:36:22 +0200

    logging feature added

--- rmonitoring.py
+++ rmonitoring.py
@@ -8,6 +8,11 @@ import platform
 from datetime import datetime
 from matplotlib import pyplot as plt
 import os
+import logging
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                    datefmt='%m-%d %H:%M')
 
 
 class Monitoring:
@@ -16,6 +21,7 @@ class Monitoring:
         """Running process (self.process) -> getting PID of process (self.pid) and passes it to psutil.Process"""
         self.process = subprocess.Popen(file_path)
         self.pid = self.process.pid
+        logging.info(f"Process {file_path} started. PID {self.pid}")
         self.py = psutil.Process(self.pid)
 
     def run_monitoring(self, interval_time):
@@ -23,6 +29,7 @@ class Monitoring:
         try:
             with open(f'{os.path.basename(process).split(".")[0]}.csv', 'w') as file:
                 if platform.system() == 'Windows':
+                    logging.info("Found Windows platform")
                     file.write(f'Date; CPU; Working Set; Private Bytes; Number of handles\n')
                     while True:
                         self.process.poll()
@@ -31,12 +38,15 @@ class Monitoring:
                             p_bytes = self.py.memory_info()[-1]
                             num_handles = self.py.num_handles()
                             cpu_use = self.py.cpu_percent()
+                            logging.info("Data collected")
                             date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
                             file.write(f'{date}; {cpu_use}; {w_set}; {p_bytes}; {num_handles} \n')
+                            logging.info("Data submitted")
                         else:
                             break
                         time.sleep(interval_time)
                 elif platform.system() == 'Linux':
+                    logging.info("Found Windows platform")
                     file.write(f'Date; CPU; Resident Set Size; Virtual Memory Size; File Descriptors\n')
                     while True:
                         self.process.poll()
@@ -45,14 +55,18 @@ class Monitoring:
                             rss = self.py.memory_info()[0]
                             vms = self.py.memory_info()[1]
                             cpu_use = self.py.cpu_percent()
+                            logging.info("Data submitted")
                             unix_date = time.time()
                             reg_date = datetime.datetime.fromtimestamp(unix_date).strftime("%Y-%m-%d %H:%M:%S")
                             file.write(f'{reg_date}; {cpu_use}; {rss}; {vms}; {f_descriptors} \n')
+                            logging.info("Data submitted")
                         else:
                             break
                         time.sleep(interval_time)
+                else:
+                    logging.error('Linux/Windows not found!')
         except psutil.NoSuchProcess:
-            print('Process closed, tracking stopped!')
+            logging.error('Process closed, tracking stopped!')
         finally:
             self.process.kill()
 
